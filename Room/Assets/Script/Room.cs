d2f556f0638f78c9b3341fc4cb362446f51d6ccf
Mirella <mirella.u@onet.pl>
2018-04-05 11:05:42 +0200

    game init

---
+++ Room/Assets/Script/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public GameObject Bear;
+
+    public void ClickTheButton()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
