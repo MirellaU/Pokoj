a86a4b16454f62b0e2b9cc784f9cda159d29ac84
Imiolek Daniel <imiolek.daniel.samuel@gmail.com>
2018-04-20 11:05:38 +0200

    MazeGame

---
+++ MazeGameProject/Assets/destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other)
+	{
+		Destroy (other.gameObject);
+		Application.LoadLevel ("Scenes");
+		Destroy (GameObject.Find ("Canvas"));
+	}
+}
